bc26b0b54259099bb607419e1578eae06a5acad2
Dailey Kaze <daileykaze@gmail.com>
2020-10-20 00:36:28 -0400

    Added class initialization

--- app.js
+++ app.js
@@ -10,7 +10,60 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function main(){
+    let users = []
+    let userId = 1
+    let managerAnswers = await inquirer.prompt([
+        {
+            message:"Please enter the manager's name",
+            name: "name"
+        },
+        {
+            message: "Please enter the manager's email",
+            name: "email"
+        },
+        {
+            message: "Please enter the manager's office number",
+            name: "officeNumber"
+        },
+        {
+            message: "Please enter the number of team members",
+            name: "teamMembers"
+        }
+    ])
+    const manager = new Manager(managerAnswers.name, userId++, managerAnswers.email, managerAnswers.officeNumber, managerAnswers.teamMembers)
+    users.push(manager)
 
+    for (let i=0; i<teamMembers; i++){
+        let employeeAnswers = await inquirer.prompt([
+            {
+                type: "list",
+                message: "What employee would you like to add?",
+                choices: ["Engineer", "Intern"],
+                name: "newEmployee"
+            }
+        ])
+        let newEmployee = employeeAnswers.newEmployee
+        let employeeInfo = await inquirer.prompt([
+            {
+                message:`Please enter the ${newEmployee}'s name`,
+                name: "name"
+            },
+            {
+                message: `Please enter the ${newEmployee}'s email`,
+                name: "email"
+            },
+            {
+                message: `Please the ${newEmployee}'s ${newEmployee=="Engineer" ? 'Github account': 'School'}?`,
+                name: "account"
+            }
+        ])
+        
+        const employee = newEmployee == "Engineer" ? new Engineer(employeeInfo.name, userId++, employeeInfo.email, employeeInfo.account) : new Intern(employeeInfo.name, userId++, employeeInfo.email, employeeInfo.account)
+        users.push(employee)
+    }
+
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
